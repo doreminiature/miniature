5690e72f4a5dccb2c2e1c5e6f2f9f6d0d56be2cc
stekolschikov <stekolschikov@mail.ua>
2017-09-03 17:10:31 +0300

    remove console log

--- js/collectionTabs.js
+++ js/collectionTabs.js
@@ -114,24 +114,12 @@ CT = {
         if (CT._openCollectionNOWClickTime + 5 < new Date() + 1) {
             try {
                 if (e.relatedTarget.classList.value == 'openCollectionNOW') {
-                    console.log('==================================================================')
-                    // console.log(e.relatedTarget.innerText)
-                    // console.log(e.relatedTarget.getAttribute("data-links"))
-                    // console.log(e.relatedTarget.getAttribute("data-links").split(","))
                     let colName = e.relatedTarget.innerText
                     let arrLinks = e.relatedTarget.getAttribute("data-links").split(",")
 
-
                     let id = tasks.addEmpty({name: colName})
                     switchToTask(id)
-                    console.log(id)
 
-
-
-                    // addTab(tabs.add({url: ''}, tabs[0]), {enterEditMode: false})
-                    // addTab(tabs.add({url: ''}, tabs[0]), {enterEditMode: false})
-                    // addTab(tabs.add({url: ''}, tabs[0]), {enterEditMode: false})
-                    // addTab(tabs.add({url: ''}, tabs[0]), {enterEditMode: false})
                     sessionRestore.save()
                     CT.render()
 
@@ -150,11 +138,6 @@ CT = {
                     if(arrLinks.length>0){
                         openURLFromsearchbar('', arrLinks[0].split(":")[0])
                     }
-                    // for (let i = 0; i < arrLinks.length; i++) {
-                    //     // console.log(arrLinks[i])
-                    //     addTabInNewCol(tabs.add({url: arrLinks[i]}, tabs[0]), {enterEditMode: false}, )
-                    // }
-                    console.log('==================================================================')
                 }
             } catch (e) {
             }

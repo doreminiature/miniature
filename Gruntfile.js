5bfea7ecaae89314bb8752251473a3bd3835fa67
nymane <positiveexperience@gmail.com>
2017-10-16 02:08:46 +0300

    Builds specifics

--- Gruntfile.js
+++ Gruntfile.js
@@ -210,13 +210,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     // grunt.loadNpmTasks('electron-reload');
 
-	grunt.registerTask('default', ['concat:browser', 'uglify:browser', 'concat:webview', 'uglify:webview', 'concat:main']);
-	grunt.registerTask('dev', ['concat:browser', 'concat:webview', 'concat:main', 'watch' , ]);
+	grunt.registerTask('default', [/*'concat:browser', 'uglify:browser', 'concat:webview', 'uglify:webview',*/ 'concat:main']);
+	grunt.registerTask('dev', [/*'concat:browser', 'concat:webview', 'concat:main',*/ 'watch' , ]);
 	grunt.registerTask('browser', ['concat:browser', 'uglify:browser']);
 	grunt.registerTask('webview', ['concat:webview', 'uglify:webview']);
 
-	grunt.registerTask('macBuild', ['concat:browser', /*'uglify:browser'*/, 'concat:webview', /*'uglify:webview'*/, 'concat:main', 'electron:osxBuild'])
-	grunt.registerTask('macBuildDev', ['concat:browser', 'concat:webview', 'concat:main', 'electron:osxBuild'])
-	grunt.registerTask('linuxBuild', ['concat:browser', /*'uglify:browser'*/, 'concat:webview', /*'uglify:webview',*/ 'concat:main', 'electron:linuxBuild', 'electron-installer-debian:linux32', 'electron-installer-debian:linux64'])
-	grunt.registerTask('windowsBuild', ['concat:browser', /* 'uglify:browser', 'uglify:webview', */ 'concat:webview', 'concat:main', 'electron:windowsBuild'])
+	grunt.registerTask('macBuild', [/*'concat:browser', 'uglify:browser', 'concat:webview', 'uglify:webview', 'concat:main',*/ 'electron:osxBuild'])
+	grunt.registerTask('macBuildDev', [/*concat:browser', 'concat:webview', 'concat:main',*/ 'electron:osxBuild'])
+	grunt.registerTask('linuxBuild', [/*'concat:browser', 'uglify:browser'*/, /*'concat:webview', 'uglify:webview', 'concat:main',*/ 'electron:linuxBuild', 'electron-installer-debian:linux32', 'electron-installer-debian:linux64'])
+	grunt.registerTask('windowsBuild', [/*'concat:browser',  'uglify:browser', 'uglify:webview',  'concat:webview', 'concat:main',*/ 'electron:windowsBuild'])
 };
